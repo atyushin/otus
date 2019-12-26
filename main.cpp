45cdf32c2afc8b791a5fcabe8aaf5cd12595bb60
ATyushin <blinkers@mail.ru>
2019-12-26 08:46:41 +0300

    complete

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "version.h"
+#include "boost/array.hpp"
+
+
+int main()
+{
+
+std::cout << "Hello World!!\n";
+std::cout << "program version: " << version() << "\n";
+
+return 0;
+}
